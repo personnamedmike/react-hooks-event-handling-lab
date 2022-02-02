ebdf661dcba348e619fd93bdc3a7dcbcb2e551fc
Mike Starr <imhs@protonmail.com>
2022-02-01 21:55:29 -0800

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  const focus = function (e) {
+    console.log("Good!");
+  };
+
+  const blur = function (e) {
+    console.log("Hey! Eyes on me!");
+  };
+
+  return (
+    <div>
+      <button onBlur={blur} onFocus={focus}>
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
